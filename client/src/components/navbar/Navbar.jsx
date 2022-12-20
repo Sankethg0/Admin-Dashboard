fb3d6a922bb92b971aabc058b3aa8e01fa332181
Sanketh Gunasekara <70938689+Sankethg0@users.noreply.github.com>
2022-12-20 16:47:11 +0530

    Design Updated

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./navbar.scss"
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
-import LanguageIcon from '@mui/icons-material/Language';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
@@ -18,7 +18,7 @@ const Navbar = () => {
         </div>
         <div className="items">
           <div className="item">
-          <LanguageIcon className='icon'/>
+            <LanguageOutlinedIcon className="icon" />
             English
           </div>
           <div className="item">
@@ -29,12 +29,21 @@ const Navbar = () => {
           </div>
           <div className="item">
             <NotificationsNoneOutlinedIcon className='icon'/>
+            <div className="counter">1</div>
           </div>
           <div className="item">
             <ChatBubbleOutlineOutlinedIcon className='icon'/>
+            <div className="counter">2</div>
           </div>
           <div className="item">
             <ListOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+          <img
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className="avatar"
+            />
           </div>
         </div>
       </div>
